75bcb2fc1fa6441ad316c9c5e48ab2064efe1623
Rahul Ghaskata <rahulghaskata01@gmail.com>
2024-06-27 22:34:33 +0530

    Added tags input for products

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,24 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TagsInput from './components/TagsInput.vue'
+import ProductTagsInput from './components/ProductTagsInput.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <HelloWorld msg="Vuejs Tags Input" />
+  <div class="container my-5">
+    <div class="d-flex align-items-center justify-content-center gap-3">
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+      <div class="wrapper">
+        <HelloWorld msg="Vue3 Tags Input" />
+      </div>
     </div>
-  </header>
-  <main>
-    <TagsInput />
-  </main>
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <TagsInput />
+      </div>
+      <div class="col-md-6 mb-3">
+        <ProductTagsInput />
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/components/ProductTagsInput.vue
+++ src/components/ProductTagsInput.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="card">
     <div class="card-header">
-      <h5>Simple Input</h5>
+      <h5>Products Tags Input</h5>
     </div>
     <div class="card-body">
       <div class="input-group gap-2">
         <input type="text" class="rounded form-control" v-model.trim="newTag" placeholder="Enter tags"
           @keypress.prevent.stop.enter="addTags" />
         <button type="button" class="rounded btn btn-sm btn-outline-dark" :disabled="!newTag"
-          @click.prevent.stop="addTags"> Add </button>
+          @click.prevent.stop="addTags"> Add
+        </button>
         <button v-if="tagsList.length > 1" type="button" class="rounded btn btn-sm btn-outline-danger"
-          @click.prevent.stop="clearTags"> Clear All </button>
+          @click.prevent.stop="clearTags"> Clear All
+        </button>
       </div>
       <span v-if="tagExists" class="text-danger mb-2">Tag is already addded in list!!!</span>
       <div class="row">
         <div class="col-auto mt-3" v-for="(tag, index) in tagsList" :key="index">
           <div class="p-2 me-2 bg-primary text-white position-relative text-wrap text-start rounded">
-            {{ tag }}
+            {{ tag.title }}
             <button class="btn btn-sm position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-              @click="removeTag(index)"> &times; </button>
+              @click="removeTag(index)">
+              &times;
+            </button>
           </div>
         </div>
       </div>
@@ -32,6 +36,10 @@
 import { ref } from "vue";
 const tagExists = ref(null);
 const newTag = ref(null);
+const selectedProduct = ref({
+  id: 'YOUR_PRODUCT_ID',
+  title: 'Amazing T-shirt'
+});
 const tagsList = ref([]);
 const removeTag = (index) => {
   tagsList.value.splice(index, 1);
@@ -44,12 +52,16 @@ const clearTags = () => {
 const addTags = () => {
   tagExists.value = false;
   tagsList.value.forEach((existingTag) => {
-    if (existingTag === newTag.value) {
+    if (existingTag.title === newTag.value) {
       tagExists.value = true;
     }
   });
   if (newTag.value && !tagExists.value) {
-    tagsList.value.push(newTag.value);
+    tagsList.value.push({
+      id: null,
+      productId: selectedProduct.value.id,
+      title: newTag.value,
+    });
     newTag.value = "";
   }
 };
